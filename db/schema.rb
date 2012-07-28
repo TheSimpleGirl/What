02a3610fd1ea5a7d6508bbbce4004b4197d1d724
Student <student@CA-iMac-05.local>
2012-07-28 15:40:03 -0500

    Builders Weekend 4 o'clock buzzer

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,9 @@ ActiveRecord::Schema.define(:version => 20120728050502) do
 
   create_table "woulds", :force => true do |t|
     t.string   "question"
-    t.string   "answer", :limit => 40
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.string   "answer",     :limit => 40
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
   end
 
 end
